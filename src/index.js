acde0b179a838deb908d95fad3d53323448865e2
smkh <113994946+masha-smkh23@users.noreply.github.com>
2024-10-01 16:23:46 +0200

    icon update in progress

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ function updateWeatherInfo(response) {
     let humidityElement = document.querySelector("#humidity"); 
     let windSpeedElement = document.querySelector("#wind-speed");
     let timeElement = document.querySelector("#time"); 
-    let date = new Date(response.data.time * 1000); 
+    let date = new Date(response.data.time * 1000);
+    let icon = 
+    console.log(response.data);
     cityElement.innerHTML = response.data.city; 
     descriptionElement.innerHTML = response.data.condition.description; 
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
